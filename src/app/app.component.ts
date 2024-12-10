232c7a038b4e2746b0b591b37c9394899608e04e
Bruno <brunotobias773@gmail.com>
2024-12-10 17:52:42 -0300

    novo projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { MatInput } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton, MatInput, MatFormFieldModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css',]
 })
 export class AppComponent {
   title = 'sistema';
+  public valor: string = ''
+
+  setNum(numero: string) {    
+    if (this.valor === null){ 
+      this.valor = numero
+    } else
+    this.valor = this.valor + numero
+  } 
+
 }
