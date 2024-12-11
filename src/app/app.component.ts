036a7056654715294d3426ec1723468ad723955e
Bruno <brunotobias773@gmail.com>
2024-12-11 13:44:33 -0300

    Subindo projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,65 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   selector: 'app-root',
   imports: [RouterOutlet, MatButton, MatInput, MatFormFieldModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css',]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'sistema';
-  public valor: string = ''
+  public temp: boolean = false;
+  public valor: string = '';
+  public segundoValor: string = '';
+  public operacao: string = '';
+  public resultado: number = 0;
 
-  setNum(numero: string) {    
-    if (this.valor === null){ 
-      this.valor = numero
-    } else
-    this.valor = this.valor + numero
-  } 
+  setNum(numero: string) {
+    if (!this.temp) {
+      if (this.valor === null) {
+        this.valor = numero;
+      } else {
+        this.valor = this.valor + numero;
+      }
+    } else {
+      if (this.segundoValor === null) {
+        this.segundoValor = numero;
+      } else {
+        this.segundoValor = this.segundoValor + numero;
+      }
+    }
+  }
 
+  setOperate(operador: string) {
+    if (this.valor == '' || this.valor == '0' || this.valor == null) {
+      alert('Defina o primeiro valor');
+    } else {
+      this.temp = true;
+      this.operacao = operador;
+    }
+  }
+
+  getResult() {
+    if (this.valor == '' || this.segundoValor == '') {
+      alert("Falta valores");
+    }
+    switch (this.operacao) {
+      case '+':
+        this.resultado = Number(this.valor) + Number(this.segundoValor);
+        break;
+      case '-':
+        this.resultado = Number(this.valor) - Number(this.segundoValor);
+        break;
+      case '*':
+        this.resultado = Number(this.valor) * Number(this.segundoValor);
+        break;
+      case '/':
+        this.resultado = Number(this.valor) / Number(this.segundoValor);
+        break;
+      default:
+        this.resultado = this.resultado;
+    }
+    this.valor = '';
+    this.segundoValor = '';
+    this.operacao = '';
+    this.temp = false;
+    alert(this.resultado);
+  }
 }
